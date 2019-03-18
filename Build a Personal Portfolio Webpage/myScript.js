a52a322a2ad5642a9a0a0ecbd3ae87d11ba34705
Hilay <1529823869@qq.com>
2019-03-19 06:50:08 +0800

    任务2“

---
+++ Build a Personal Portfolio Webpage/myScript.js
@@ -0,0 +1,41 @@
+window.onload= function() {
+    var li = document.querySelectorAll("ul li");
+    window.onscroll = function() {
+        var top = document.documentElement.scrollTop;
+        var items = document.querySelectorAll(".item");
+        var currentId = "";
+
+        items.forEach(function(a) {
+            var itemTop = a.offsetTop;
+            if(top>=itemTop) {
+                currentId = "#"+a.getAttribute("id");
+            }
+            else {
+                return false;
+            }
+        });
+
+        console.log(currentId);
+        //进行样式的解绑
+
+        var activeItem = document.querySelector("li.active");
+        if(currentId && currentId!==activeItem.childNodes[0].href) {
+
+            var itemClassName = activeItem.className.split(" ");
+
+            itemClassName.pop();
+            itemClassName = itemClassName.join(" ");
+            activeItem.className = itemClassName;
+            console.log(itemClassName);
+            console.log(activeItem.className);
+
+            //进行样式的绑定
+            for(var a of li) {
+                console.log(a.childNodes[0]);
+                if(a.childNodes[0].getAttribute("href")===currentId) {
+                    a.className+=" active";
+                }
+            }
+        }
+  };
+};
---
+++ Build a Tribute Page/test.js
@@ -0,0 +1,6 @@
+var regex1 = /foo*/g;
+
+var str1 = "table footablall foootal tdjaltoooofooo";
+var array1;
+array1 = regex1.exec(str1);
+console.log(array1);
